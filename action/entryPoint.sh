8d58fff755eaab5ae627454f4e7ddd0950c21958
mashuk999 <mashuk1998@gmail.com>
2020-10-30 19:14:22 +0530

    Some Changes

--- action/entryPoint.sh
+++ action/entryPoint.sh
@@ -2,24 +2,38 @@
 
 pwd
 
-unity-editor -nographics \
-     -manualLicenseFile licence2018.ulf \
-     -logfile
+mkdir -p "./Assets/Editor/"
 
-unity-editor -nographics \
-    -logfile /dev/stdout \
-    -quit \
-    -customBuildName "1" \
-    -projectPath "." \
-    -buildTarget "Android" \
-    -customBuildTarget "Android" \
-    -customBuildPath "build/Android/Android.aab" \
-    -executeMethod "UnityBuilderAction.Builder.BuildProject" \
-    -buildVersion "1" \
-    -androidVersionCode "1" \
-    -androidKeystoreName "asterdash.keystore" \
-    -androidKeystorePass "withoutlogic" \
-    -androidKeyaliasName "abhay nigam" \
-    -androidKeyaliasPass "withoutlogic"
+cp -R "/Editor/" "./Assets/Editor/"
+
+ls
+
+cd Assets
+
+ls
+
+cd Editor
+
+ls
+
+# unity-editor -nographics \
+#      -manualLicenseFile licence2018.ulf \
+#      -logfile
+
+# unity-editor -nographics \
+#     -logfile /dev/stdout \
+#     -quit \
+#     -customBuildName "1" \
+#     -projectPath "." \
+#     -buildTarget "Android" \
+#     -customBuildTarget "Android" \
+#     -customBuildPath "build/Android/Android.aab" \
+#     -executeMethod "UnityBuilderAction.Builder.BuildProject" \
+#     -buildVersion "1" \
+#     -androidVersionCode "1" \
+#     -androidKeystoreName "asterdash.keystore" \
+#     -androidKeystorePass "withoutlogic" \
+#     -androidKeyaliasName "abhay nigam" \
+#     -androidKeyaliasPass "withoutlogic"
 
 
